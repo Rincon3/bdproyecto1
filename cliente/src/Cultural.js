717ed8f9130956a4e3220b6f15ccf5456b635e27
EDUARDO JOSE DAZA PALENCIA <eduardo.daza@correounivalle.edu.co>
2021-09-28 15:57:06 -0500

    cambios cultural

--- cliente/src/Cultural.js
+++ cliente/src/Cultural.js
@@ -16,14 +16,8 @@ export const Cultural = () => {
   const [localidad, setLocalidad] = useState('')
   const [cantidad_pago, setCantidad_pago] = useState('')
 
-<<<<<<< HEAD
-  
-
-
-=======
   const buttonEnviar = document.querySelector(".btnEnviar");
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
     const guardabase = async () => {
         const res = await axios.post('/basedatos/insertarEncuestatipo', {nombre_evento, asistencia, numero_asistencia, 
           participacion, pago, medio_pago, duracion, localidad, cantidad_pago});
@@ -37,48 +31,6 @@ export const Cultural = () => {
         setDuracion('')
         setLocalidad('')
         setCantidad_pago('')
-<<<<<<< HEAD
-        
-
-
-      }
-    const  consultabase = async () => {
-        const res = await axios.get('/basedatos/consultatotalpacientes');
-        console.log('data api',res.data)
-        return res
-      }
-
-      const actualizabase = async () => {
-        const res = await axios.put('/basedatos/actualizarpacientes', {numid: documento, nombre, apellido });
-        console.log(res.data)
-        setDocumento('')
-        setNombre('')
-        setApellido('')
-      }
-
-      const  eliminabase = async () => {
-        const res = await axios.delete('/basedatos/eliminarpacientes', {data: {numid: documento}});
-        console.log('data api',res.data)
-        return res
-      }
-
-      
-    
-      const onChangedc = (e) => {
-        setDocumento(e.currentTarget.value);
-        console.log(documento)
-      };
-    
-      const onChangenm = (e) => {
-        setNombre(e.currentTarget.value);
-        console.log(nombre)
-      };  
-
-      const onChangeap = (e) => {
-        setApellido(e.currentTarget.value)
-        
-=======
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
       }
       
       //Onchange agregado para nombre_evento
@@ -100,56 +52,32 @@ export const Cultural = () => {
         setParticipacion(e.currentTarget.value);
         console.log(participacion)
       }
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
       const onchangePago = (e) => {
         setPago(e.currentTarget.value);
         console.log(pago)
       }
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
       const onchangeMedio_pago = (e) => {
         setMedio_pago(e.currentTarget.value);
         console.log(medio_pago)
       }
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
       const onchangeDuracion = (e) => {
         setDuracion(e.currentTarget.value);
         console.log(duracion)
       }
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
       const onchangeLocalidad = (e) => {
         setLocalidad(e.currentTarget.value);
         console.log(localidad)
       }
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
       const onchangeCantidad_pago = (e) => {
         setCantidad_pago(e.currentTarget.value);
         console.log(cantidad_pago)
       }
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
     const inserta = () => {
         console.log('Se hizo click');
          guardabase()
@@ -166,20 +94,16 @@ export const Cultural = () => {
             
     return (
         <div  className="formdb__box-containter">
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
             <h3 className="auth__title">CULTURAL</h3>
             
             <FormGroup>
                 <Label for="CheckboxNombre_evento">¿Qué evento cultural te gusta más?</Label>
                 <div>
-                  <CustomInput type="radio" id="radioNombre_evento1" label="Concierto" value="futbol" checked={esSeleccionado(nombre_evento, "futbol")} onChange={onchangeNombre_evento} />
-                  <CustomInput type="radio" id="radioNombre_evento2" label="Teatro" value="ciclismo" checked={esSeleccionado(nombre_evento, "ciclismo")} onChange={onchangeNombre_evento} />
-                  <CustomInput type="radio" id="radioNombre_evento3" label="Peliculas" value="lucha libre" checked={esSeleccionado(nombre_evento, "lucha libre")} onChange={onchangeNombre_evento} />
-                  <CustomInput type="radio" id="radioNombre_evento4" label="Danza" value="automovilismo" checked={esSeleccionado(nombre_evento, "automovilismo")} onChange={onchangeNombre_evento} />
+                  <CustomInput type="radio" id="radioNombre_evento1" label="Concierto" value="concierto" checked={esSeleccionado(nombre_evento, "concierto")} onChange={onchangeNombre_evento} />
+                  <CustomInput type="radio" id="radioNombre_evento2" label="Teatro" value="teatro" checked={esSeleccionado(nombre_evento, "teatro")} onChange={onchangeNombre_evento} />
+                  <CustomInput type="radio" id="radioNombre_evento3" label="Peliculas" value="peliculas" checked={esSeleccionado(nombre_evento, "peliculas")} onChange={onchangeNombre_evento} />
+                  <CustomInput type="radio" id="radioNombre_evento4" label="Danza" value="danza" checked={esSeleccionado(nombre_evento, "danza")} onChange={onchangeNombre_evento} />
                 </div>
               </FormGroup>
  
@@ -200,11 +124,7 @@ export const Cultural = () => {
                   <CustomInput type="radio" id="radioNumero_asistencia3" label="6 a mas veces" value="6 a mas veces" checked={esSeleccionado(numero_asistencia, "6 a mas veces")} onChange={onchangeNumero_asitencia} />
                 </div>
               </FormGroup>
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
               <FormGroup>
                 <Label for="CheckboxParticipacion">¿Le gustaría participar en este evento?</Label>
                 <div>
@@ -212,11 +132,7 @@ export const Cultural = () => {
                   <CustomInput type="radio" id="radioParticipacion2" label="No" value={false} checked={esSeleccionado(participacion, "false")} onChange={onchangeParticipacion} />
                 </div>
               </FormGroup>
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
               <FormGroup>
                 <Label for="CheckboxPago">¿Pagarías por asistir a este evento?</Label>
                 <div>
@@ -224,11 +140,7 @@ export const Cultural = () => {
                   <CustomInput type="radio" id="radioPago2" label="No" value={false} checked={esSeleccionado(pago, "false")} onChange={onchangePago} />
                 </div>
               </FormGroup>
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
               <FormGroup>
                 <Label for="CheckboxMedio_pago">¿Por cuál medio de pago te gustaría pagar el ticket?</Label>
                 <div>
@@ -236,11 +148,7 @@ export const Cultural = () => {
                   <CustomInput type="radio" id="radioMedio_pago2" label="Online" value="online" checked={esSeleccionado(medio_pago, "online")} onChange={onchangeMedio_pago} />
                 </div>
               </FormGroup>
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
               <FormGroup>
                 <Label for="CheckboxDuracion">¿A cuantos eventos de deporte has ido en el último año?</Label>
                 <div>
@@ -249,17 +157,6 @@ export const Cultural = () => {
                   <CustomInput type="radio" id="radioDuracion3" label="5 o más horas" value="5 o más horas" checked={esSeleccionado(duracion, "5 o más horas")} onChange={onchangeDuracion} />
                 </div>
               </FormGroup>
-<<<<<<< HEAD
-
-              <FormGroup>
-                <Label for="CheckboxLocalidad">¿Te gustaría que este evento se lleve a cabo en tu localidad?</Label>
-                <div>
-                  <CustomInput type="radio" id="radioLocalidad1" label="Si" value={true} checked={esSeleccionado(pago, "true")} onChange={onchangeLocalidad} />
-                  <CustomInput type="radio" id="radioLocalidad2" label="No" value={false} checked={esSeleccionado(pago, "false")} onChange={onchangeLocalidad} />
-                </div>
-              </FormGroup>
-
-=======
  
               <FormGroup>
                 <Label for="CheckboxLocalidad">¿Te gustaría que este evento se lleve a cabo en tu localidad?</Label>
@@ -269,7 +166,6 @@ export const Cultural = () => {
                 </div>
               </FormGroup>
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
               <FormGroup>
                 <Label for="CheckboxCantidad_pago">¿Cuánto estarías dispuesto a pagar por este evento?</Label>
                 <div>
@@ -278,11 +174,7 @@ export const Cultural = () => {
                   <CustomInput type="radio" id="radioCantidad_pago3" label="100k o más" value="100k o más" checked={esSeleccionado(cantidad_pago, "100k o más")} onChange={onchangeCantidad_pago} />
                 </div>
               </FormGroup>
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
                 
                 
                 <button
@@ -325,10 +217,7 @@ export const Cultural = () => {
                 type="button">
                   Sección Danza
                 </button>
-<<<<<<< HEAD
-=======
                 </Link>
->>>>>>> 6c71193edc1ac398062048dd819ac5e1d42dc8f0
             
         </div>
     )
